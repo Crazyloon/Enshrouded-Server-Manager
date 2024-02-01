c5d694e96e39928e180337ca4ceae55dd8a5dfd1
ISpaikI <88611660+ISpaikI@users.noreply.github.com>
2024-02-01 17:04:27 +0100

    discord integration

---
+++ Enshrouded Server Manager/Services/DiscordOutput.cs
@@ -0,0 +1,67 @@
+﻿using Discord;
+using Discord.Webhook;
+
+namespace Enshrouded_Server_Manager.Services;
+
+class DiscordOutput
+{
+    // send server is online status to webhook
+    public async Task ServerOnline(string SERVERNAME, string URL)
+    {
+        // The webhook url follows the format https://discord.com/api/webhooks/{id}/{token}
+        using var client = new DiscordWebhookClient($"{URL}");
+
+        var embed = new EmbedBuilder
+        {
+            Title = $"Server {SERVERNAME}",
+            Description = "Server is online!"
+        };
+
+        await client.SendMessageAsync(text: "", embeds: new[] { embed.Build() });
+    }
+
+    // send server is offline status to webhook
+    public async Task ServerOffline(string SERVERNAME, string URL)
+    {
+        // The webhook url follows the format https://discord.com/api/webhooks/{id}/{token}
+        using var client = new DiscordWebhookClient($"{URL}");
+
+        var embed = new EmbedBuilder
+        {
+            Title = $"Server {SERVERNAME}",
+            Description = "Server is offline!"
+        };
+
+        await client.SendMessageAsync(text: "", embeds: new[] { embed.Build() });
+    }
+
+    // send server is updating status to webhook
+    public async Task ServerUpdating(string SERVERNAME, string URL)
+    {
+        // The webhook url follows the format https://discord.com/api/webhooks/{id}/{token}
+        using var client = new DiscordWebhookClient($"{URL}");
+
+        var embed = new EmbedBuilder
+        {
+            Title = $"Server {SERVERNAME}",
+            Description = "Server is updating!"
+        };
+
+        await client.SendMessageAsync(text: "", embeds: new[] { embed.Build() });
+    }
+
+    // send server backup has been created status to webhook
+    public async Task ServerBackup(string SERVERNAME, string URL)
+    {
+        // The webhook url follows the format https://discord.com/api/webhooks/{id}/{token}
+        using var client = new DiscordWebhookClient($"{URL}");
+
+        var embed = new EmbedBuilder
+        {
+            Title = $"Server {SERVERNAME}",
+            Description = "AutoBackup created!"
+        };
+
+        await client.SendMessageAsync(text: "", embeds: new[] { embed.Build() });
+    }
+}
