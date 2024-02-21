d42c2c55a492050eaec2a6335e3d4f9ef7e36ea3
Russell Dow <russdow1@hotmail.com>
2024-02-21 14:46:07 -0800

    Added config to determine which UI to use

--- Enshrouded Server Manager/Program.cs
+++ Enshrouded Server Manager/Program.cs
@@ -7,38 +7,29 @@ internal static class Program
     {
         ApplicationConfiguration.Initialize();
 
-        //Application.Run(new MainForm());
-        Application.Run(new NewUIForm());
+        var uiVersion = File.ReadAllLines("./config.txt").Where(x => x.StartsWith("UI")).ToList().FirstOrDefault();
+
+        if (uiVersion is null)
+        {
+            Application.Run(new NewUIForm());
+        }
+        else
+        {
+            var split = uiVersion.Split('=');
+            if (split.Length == 2)
+            {
+                if (split[0] == "UI")
+                {
+                    if (split[1] == "NEW")
+                    {
+                        Application.Run(new NewUIForm());
+                    }
+                    else
+                    {
+                        Application.Run(new MainForm());
+                    }
+                }
+            }
+        }
     }
-
-    //public static IServiceProvider ServiceProvider { get; private set; }
-    //static IHostBuilder CreateHostBuilder()
-    //{
-    //    Dictionary<string, CountDownTimer> restartTimers = new();
-
-    //    return Host.CreateDefaultBuilder()
-    //        .ConfigureServices((_, services) =>
-    //        {
-    //            services.AddTransient<IBackupService, BackupService>();
-    //            services.AddTransient<IDiscordService, DiscordService>();
-    //            services.AddTransient<IEnshroudedServerService, EnshroudedServerService>();
-    //            services.AddTransient<IFileLoggerService, FileLogger>();
-    //            services.AddTransient<IFileSystemService, FileSystemService>();
-    //            services.AddTransient<WebClient>();
-    //            services.AddTransient<IHttpClientService, HttpClientService>();
-    //            services.AddTransient<IMessageBoxService, MessageBoxService>();
-    //            services.AddTransient<IProfileService, ProfileService>();
-    //            services.AddTransient<IServerSettingsService, ServerSettingsService>();
-    //            services.AddTransient<ISteamCMDInstallerService, SteamCMDInstallerService>();
-    //            services.AddTransient<ISystemProcessService, SystemProcessService>();
-    //            services.AddTransient<IVersionManagementService, VersionManagementService>();
-    //            services.AddTransient<IScheduledRestartService, ScheduledRestartService>();
-
-    //            services.AddTransient<MainForm>();
-
-    //            services.AddSingleton<Dictionary<string, CountDownTimer>>(new Dictionary<string, CountDownTimer>());
-
-    //            services.AddSingleton<IEventAggregator, EventAggregator>();
-    //        });
-    //}
 }
